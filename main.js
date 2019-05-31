6e6eeba3267720f2564c0f6bd4d4ca4cdfe6a6a4
Rahul Sahu <rahulsahu.blog@gmail.com>
2019-05-31 15:42:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -31,21 +31,14 @@ function get() {
 
 
     hours = Math.floor(distance /(3600000));
-    if(hours < 10){
-        hours = "0" + hours;
-    }
     minutes = Math.floor((distance % (3600000)) / (60000));
-    if (minutes < 10){
-        minutes = "0" + minutes;
-    }
     seconds = Math.floor((distance % (60000)) / 1000);
-    if(seconds < 10){
-        seconds = "0" + seconds;
-    }
-    milliseconds = Math.floor((distance % 1000)/ 10);
-    if(milliseconds<10){
-        milliseconds = "0" + milliseconds;
-    }
+    milliseconds = Math.floor((distance % 1000)/ 10);   
+
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+    milliseconds = (milliseconds < 10) ? "0" + milliseconds : milliseconds;
 
     timer.innerHTML = hours + ":" + minutes + ":" + seconds + ":" + milliseconds;
 
